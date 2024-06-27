ab1a254eb1c5a5491dcb7815a5e8461a981ad393
Nathan O. <46687695+N4te2k@users.noreply.github.com>
2024-06-27 21:20:00 +0200

    added documentation

--- obj/Debug/net8.0-windows/SchnaeppchenJaeger.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/SchnaeppchenJaeger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchnaeppchenJaeger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af4af66759816dd26036a60207e798cff1e7514e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a77eb6aadfbb98ef33fb3324e6fa00c6e0f518")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchnaeppchenJaeger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchnaeppchenJaeger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
